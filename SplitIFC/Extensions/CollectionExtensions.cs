91f863d57a44635e40c61516c0170698d3bbee4a
Phong Huỳnh <closeheart777@live.com>
2023-04-08 22:57:50 +0700

    abc

---
+++ SplitIFC/Extensions/CollectionExtensions.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SplitIFC.Extensions
+{
+    public static class CollectionExtensions
+    {
+        public static List<List<T>> ChunkBy<T>(this List<T> source, int chunkSize)
+        {
+            return source
+                .Select((x, i) => new { Index = i, Value = x })
+                .GroupBy(x => x.Index / chunkSize)
+                .Select(x => x.Select(v => v.Value).ToList())
+                .ToList();
+        }
+        public static List<List<T>> Divide<T>(this List<T> array, int size)
+        {
+            List<List<T>> result = new List<List<T>>();
+            int numbersInGroup = array.Count / size;
+            int numbersInTheory = numbersInGroup * size;
+            for (int i = 0; i < size; i++)
+            {
+                List<T> group = new List<T>();
+                if (numbersInTheory < array.Count && i==size-1)
+                {
+                    group = array.Skip(i * numbersInGroup).Take(numbersInGroup + array.Count - numbersInTheory).ToList();
+
+                }
+                else
+                {
+                    group = array.Skip(i * numbersInGroup).Take(numbersInGroup).ToList();
+
+                }
+                result.Add(group);
+            }
+            return result;
+        }
+    }
+}
---
+++ SplitIFC/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+using IfcToolbox.Core.Editors;
+using IfcToolbox.Core.Utilities;
+using IfcToolbox.Tools.Configurations;
+using IfcToolbox.Tools.Processors;
+using SplitIFC.Extensions;
+using Xbim.Ifc;
+using Xbim.Ifc4.Interfaces;
+
+//Console.WriteLine("Hello, World!");
+string filePath = "concept2.ifc";
+//IConfigSplit config = ConfigFactory.CreateConfigSplit();
+//config.LogDetail = true;
+//config.SplitStrategy = SplitStrategy.ByBuildingStorey;
+//config.SelectedItems = new List<string> { "137", "131" };
+//SplitterProcessor.Process(filePath, config, true);
+using (var model = IfcStore.Open(filePath))
+{
+    var requiredProducts = model.Instances.OfType<IIfcProduct>().Where(x => !(x is IIfcSpatialStructureElement)).ToList();
+    var listListProducts  = requiredProducts.Divide(20);
+    for (int i = 0; i < listListProducts.Count(); i++)
+    {
+        var items = listListProducts[i];
+        string path = Path.Combine("D:\\Github\\IfcToolbox\\SplitIFC\\bin\\Debug\\net6.0\\output", $"concept2{i}.ifc");
+        InsertCopy.CopyProducts(model, path, listListProducts[i], true);
+    }
+
+
+}
